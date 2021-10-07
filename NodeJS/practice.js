8d16d74626ce75c22990610b39c34d7717c326cf
shubhamkd <shubham.k@browserstack.com>
2021-10-07 15:50:15 +0530

    kuch toh h

---
+++ NodeJS/practice.js
@@ -0,0 +1,11 @@
+// console.log("o bhailu, kaisan h??");
+
+// const EventEmitter = require('events');
+// const eventEmitter= new EventEmitter();
+
+// eventEmitter.on('tut',(x,y)=>{console.log(x+y)});
+
+// eventEmitter.emit('tut',5,7);
+
+const chilka= Math.random().toString(10);
+console.log(chilka);
